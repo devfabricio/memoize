016c764bc0208e8f06890906fa3a20ac30b003d5
Fabrício Augusto <fabricio.mata@gmail.com>
2021-05-27 12:17:39 -0300

    solution

--- src/memo.js
+++ src/memo.js
@@ -1,5 +1,17 @@
 export function memoize(fn) {
-  // implement here
+  const cache = new Map();
+  return (...params) => {
+    const key = JSON.stringify(params);
+
+    if(cache.has(key)) {
+      console.log(`Get from cache ${params}`)
+      return cache.get(key)
+    } else {
+      const result = fn(...params)
+      cache.set(key, result)
+      return result
+    }
+  }
 }
 
 export function cache(fn) {
